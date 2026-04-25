e1cd03b1aacce3025bbde00ec14a196e4b584d93
sofiarayon <156807068+sofiarayon@users.noreply.github.com>
2026-04-25 19:48:03 -0400

    Improve Map
    update CampusMap scene and resources to improve UI. Changes include: add DarkOverlay ColorRect, replace HButton with HArea + HLabel and theme/font overrides (uses new font), adjust MapButton/MapPanel/CloseButton layout, update atlas region_rects in ui.tres and ui.png, and wire new signals for hover/pressed to show H label and open H scene. Script updated to toggle the dark overlay and H label visibility accordingly.

--- Scenes/UI/campus_map.gd
+++ Scenes/UI/campus_map.gd
@@ -1,18 +1,30 @@
 extends CanvasLayer
 
 @onready var map_panel: Panel = $MapPanel
+@onready var dark_overlay = $DarkOverlay
 
 func _ready() -> void:
 	map_panel.visible = false
+	dark_overlay.visible = false
+	$MapPanel/HLabel.visible = false
+	
 
 func _on_map_button_pressed() -> void:
+	dark_overlay.visible = true
 	map_panel.visible = true
 
 func _on_close_button_pressed() -> void:
 	map_panel.visible = false
+	dark_overlay.visible = false
 
 func _on_l_button_pressed() -> void:
 	SceneManager.enter_scene("res://Scenes/Interiors/L_Interior.tscn")
 
-func _on_h_button_pressed() -> void:
+func _on_h_area_pressed() -> void:
 	SceneManager.enter_scene("res://Scenes/Exteriors/H_Exterior.tscn")
+
+func _on_h_area_mouse_entered() -> void:
+	$MapPanel/HLabel.visible = true
+
+func _on_h_area_mouse_exited() -> void:
+	$MapPanel/HLabel.visible = false
